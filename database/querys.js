df720899b7a008252977d02ff4cd1350b8482ccd
matias25pinto <matias25pinto@gmail.com>
2022-03-01 13:12:26 -0300

    ADD(producto):Se agrego codigoInterno a las consultas de producto

--- database/querys.js
+++ database/querys.js
@@ -4,79 +4,84 @@ const sqlConfig = require("./config");
 const { fechaQuery } = require("../helpers/formatear-fecha");
 
 const productosMasVendidosDelDia = async () => {
-	try {
-		//agregamos zona horaria de paraguay
-		let fechaString = new Date().toLocaleString("es-PY", {
-			timeZone: "America/Asuncion",
-		});
-		//La fecha se convierte a un string que vamos transformando
-		let fechaArray = fechaString.split("/");
-		let yearHour = fechaArray[2].split(" ");
-		let year = yearHour[0];
-		let month = fechaArray[1];
-		let day = fechaArray[0];
-		if (month < 10) {
-			month = "0" + month.toString();
-		}
-		if (day < 10) {
-			day = "0" + day.toString();
-		}
-		const condicion = year.toString() + month + day;
-		// make sure that any items are correctly URL encoded in the connection string
-		await sql.connect(sqlConfig);
-		const result =
-			await sql.query`SELECT codigobarra, nombreproducto, SUM(cantidad) as "cantidad" 
-                                        FROM VENTAS_SMARKET_VIEW 
-                                        WHERE fecha = ${condicion} 
-                                        GROUP BY codigobarra, nombreproducto 
+  try {
+    //agregamos zona horaria de paraguay
+    let fechaString = new Date().toLocaleString("es-PY", {
+      timeZone: "America/Asuncion",
+    });
+    //La fecha se convierte a un string que vamos transformando
+    let fechaArray = fechaString.split("/");
+    let yearHour = fechaArray[2].split(" ");
+    let year = yearHour[0];
+    let month = fechaArray[1];
+    let day = fechaArray[0];
+    if (month < 10) {
+      month = "0" + month.toString();
+    }
+    if (day < 10) {
+      day = "0" + day.toString();
+    }
+    const condicion = year.toString() + month + day;
+    // make sure that any items are correctly URL encoded in the connection string
+    await sql.connect(sqlConfig);
+    const result =
+      await sql.query`SELECT v.codigobarra, v.nombreproducto, p.codigo as "codigoInterno", SUM(v.cantidad) as "cantidad" 
+                                        FROM VENTAS_SMARKET_VIEW v
+					JOIN Producto p
+					ON v.codigobarra = p.codigobarra
+                                        WHERE v.fecha = ${condicion} 
+										AND p.empresa = 4
+                                        GROUP BY v.codigobarra, v.nombreproducto, p.codigo 
                                         ORDER BY 3 DESC
                                         OFFSET 0 ROWS
                                         FETCH FIRST 10 ROWS ONLY`;
-		let productos = [];
-		for (let row of result.recordset) {
-			productos.push({
-				codigoBarra: row.codigobarra,
-				producto: row.nombreproducto,
-			});
-		}
-		return productos;
-	} catch (err) {
-		console.log("ERROR!!! no se pudo realizar la consulta", err);
-		return [];
-		// ... error checks
-	}
+    let productos = [];
+    for (let row of result.recordset) {
+      productos.push({
+        codigoBarra: row.codigobarra,
+        codigoInterno: row.codigoInterno,
+        producto: row.nombreproducto,
+      });
+    }
+    return productos;
+  } catch (err) {
+    console.log("ERROR!!! no se pudo realizar la consulta", err);
+    return [];
+    // ... error checks
+  }
 };
 
 const nuevosProductos = async () => {
-	try {
-		await sql.connect(sqlConfig);
-		const result = await sql.query`SELECT codigobarra, descripcion, empresa
-                                               FROM Producto 
+  try {
+    await sql.connect(sqlConfig);
+    const result =
+      await sql.query`SELECT p.codigobarra, p.codigo as "codigoInterno", p.descripcion, empresa
+                                               FROM Producto p
                                                WHERE empresa = 4
                                                ORDER BY id DESC
                                                OFFSET 0 ROWS 
                                                FETCH FIRST 10 ROWS ONLY`;
-		let productos = [];
-		for (let row of result.recordset) {
-			productos.push({
-				codigoInterno: row.codigo,
-				codigoBarra: row.codigobarra,
-				producto: row.descripcion,
-			});
-		}
+    let productos = [];
+    for (let row of result.recordset) {
+      productos.push({
+        codigoInterno: row.codigoInterno,
+        codigoBarra: row.codigobarra,
+        producto: row.descripcion,
+      });
+    }
 
-		return productos;
-	} catch (err) {
-		console.log("ERROR!!! no se pudo realizar la consulta", err);
-		return [];
-	}
+    return productos;
+  } catch (err) {
+    console.log("ERROR!!! no se pudo realizar la consulta", err);
+    return [];
+  }
 };
 
 const buscarProductos = async (termino, iddeposito, desde, hasta) => {
-	try {
-		await sql.connect(sqlConfig);
-		const result =
-			await sql.query`SELECT p.id, p.codigobarra, p.descripcion, p.descripcionale, p.empresa, s.iddeposito, d.codigo, SUM(s.existencia) as "existencia", p.pcosto, p.pcostod, p.preven, p.preven2, p.preven3, p.preven4, p.preven5, p.preven6, p.preven7, p.preven8, p.preven9, p.preven10, p.preven11, p.preven12, p.preven13, p.preven14, p.preven15, p.preven16, p.preven17, p.preven18, p.preven19, p.preven20
+  try {
+    await sql.connect(sqlConfig);
+    const result =
+      await sql.query`SELECT p.id, p.codigobarra,  p.codigo as "codigoInterno", p.descripcion, p.descripcionale, p.empresa, s.iddeposito, d.codigo, SUM(s.existencia) as "existencia", p.pcosto, p.pcostod, p.preven, p.preven2, p.preven3, p.preven4, p.preven5, p.preven6, p.preven7, p.preven8, p.preven9, p.preven10, p.preven11, p.preven12, p.preven13, p.preven14, p.preven15, p.preven16, p.preven17, p.preven18, p.preven19, p.preven20
    FROM Producto p
     INNER JOIN STKDeposito s
     ON p.id = s.idproducto
@@ -85,34 +90,34 @@ const buscarProductos = async (termino, iddeposito, desde, hasta) => {
     WHERE p.empresa = 4
     AND s.iddeposito = ${iddeposito}
     AND PATINDEX ('%' + CAST(${termino} AS VARCHAR) +'%',p.descripcion)  !=0
-    GROUP BY p.id, p.codigobarra, p.descripcion, p.descripcionale, p.empresa, s.iddeposito, d.codigo, p.pcostod, p.pcosto, p.preven, p.preven2, p.preven3, p.preven4, p.preven5, p.preven6, p.preven7, p.preven8, p.preven9, p.preven10, p.preven11, p.preven12, p.preven13, p.preven14, p.preven15, p.preven16, p.preven17, p.preven18, p.preven19, p.preven20
+    GROUP BY p.id, p.codigobarra, p.codigo, p.descripcion, p.descripcionale, p.empresa, s.iddeposito, d.codigo, p.pcostod, p.pcosto, p.preven, p.preven2, p.preven3, p.preven4, p.preven5, p.preven6, p.preven7, p.preven8, p.preven9, p.preven10, p.preven11, p.preven12, p.preven13, p.preven14, p.preven15, p.preven16, p.preven17, p.preven18, p.preven19, p.preven20
     ORDER BY 1 DESC
     OFFSET ${desde} ROWS
     FETCH FIRST ${hasta} ROWS ONLY
     `;
-		let productos = [];
-		for (let row of result.recordset) {
-			productos.push(row);
-		}
+    let productos = [];
+    for (let row of result.recordset) {
+      productos.push(row);
+    }
 
-		return productos;
-	} catch (err) {
-		console.log("ERROR!!! no se pudo realizar la consulta", err);
-		return [];
-	}
+    return productos;
+  } catch (err) {
+    console.log("ERROR!!! no se pudo realizar la consulta", err);
+    return [];
+  }
 };
 
 const buscarProductosCB = async (
-	termino,
-	longitud,
-	iddeposito,
-	desde,
-	hasta
+  termino,
+  longitud,
+  iddeposito,
+  desde,
+  hasta
 ) => {
-	try {
-		await sql.connect(sqlConfig);
-		const result =
-			await sql.query`SELECT p.id, p.codigobarra, p.descripcion, p.descripcionale, p.empresa, s.iddeposito, d.codigo, SUM(s.existencia) as "existencia",p.pcostod, p.pcosto, p.preven, p.preven2, p.preven3, p.preven4, p.preven5, p.preven6, p.preven7, p.preven8, p.preven9, p.preven10, p.preven11, p.preven12, p.preven13, p.preven14, p.preven15, p.preven16, p.preven17, p.preven18, p.preven19, p.preven20
+  try {
+    await sql.connect(sqlConfig);
+    const result =
+      await sql.query`SELECT p.id, p.codigobarra, p.codigo as "codigoInterno", p.descripcion, p.descripcionale, p.empresa, s.iddeposito, d.codigo, SUM(s.existencia) as "existencia",p.pcostod, p.pcosto, p.preven, p.preven2, p.preven3, p.preven4, p.preven5, p.preven6, p.preven7, p.preven8, p.preven9, p.preven10, p.preven11, p.preven12, p.preven13, p.preven14, p.preven15, p.preven16, p.preven17, p.preven18, p.preven19, p.preven20
     FROM Producto p
     INNER JOIN STKDeposito s
     ON p.id = s.idproducto
@@ -121,30 +126,30 @@ const buscarProductosCB = async (
     WHERE p.empresa = 4
     AND s.iddeposito = ${iddeposito}
     AND RIGHT(p.codigobarra, ${longitud}) = ${termino}
-    GROUP BY p.id, p.codigobarra, p.descripcion, p.descripcionale, p.empresa, s.iddeposito, d.codigo, p.pcostod, p.pcosto, p.preven, p.preven2, p.preven3, p.preven4, p.preven5, p.preven6, p.preven7, p.preven8, p.preven9, p.preven10, p.preven11, p.preven12, p.preven13, p.preven14, p.preven15, p.preven16, p.preven17, p.preven18, p.preven19, p.preven20
+    GROUP BY p.id, p.codigobarra, p.codigo, p.descripcion, p.descripcionale, p.empresa, s.iddeposito, d.codigo, p.pcostod, p.pcosto, p.preven, p.preven2, p.preven3, p.preven4, p.preven5, p.preven6, p.preven7, p.preven8, p.preven9, p.preven10, p.preven11, p.preven12, p.preven13, p.preven14, p.preven15, p.preven16, p.preven17, p.preven18, p.preven19, p.preven20
     ORDER BY 1 DESC
     OFFSET ${desde} ROWS
     FETCH FIRST ${hasta} ROWS ONLY`;
-		let productos = [];
-		for (let row of result.recordset) {
-			productos.push(row);
-		}
-		return productos;
-	} catch (err) {
-		console.log("ERROR!!! no se pudo realizar la consulta", err);
-		return [];
-	}
+    let productos = [];
+    for (let row of result.recordset) {
+      productos.push(row);
+    }
+    return productos;
+  } catch (err) {
+    console.log("ERROR!!! no se pudo realizar la consulta", err);
+    return [];
+  }
 };
 
 const calcularArqueo = async (desde = new Date(), hasta = new Date()) => {
-	try {
-		const fecha1 = fechaQuery(desde);
-		const fecha2 = fechaQuery(hasta);
+  try {
+    const fecha1 = fechaQuery(desde);
+    const fecha2 = fechaQuery(hasta);
 
-		// make sure that any items are correctly URL encoded in the connection string
-		await sql.connect(sqlConfig);
-		const result =
-			await sql.query`SELECT su.codigo AS "codigosucursal", ltrim(rtrim(su.descripcion)) nombresucursal ,  SUM(a.cantidad) AS "cantidad", 
+    // make sure that any items are correctly URL encoded in the connection string
+    await sql.connect(sqlConfig);
+    const result =
+      await sql.query`SELECT su.codigo AS "codigosucursal", ltrim(rtrim(su.descripcion)) nombresucursal ,  SUM(a.cantidad) AS "cantidad", 
                                         SUM(pr.pcosto*a.cantidad) AS "totalCosto", 
                                         SUM((ROUND(a.precioneto,0))*a.cantidad) AS "totalVentas", 
                                         SUM(((ROUND(a.precioneto,0))*a.cantidad)-(pr.pcosto*a.cantidad)) AS "totalUtilidad"
@@ -159,28 +164,28 @@ const calcularArqueo = async (desde = new Date(), hasta = new Date()) => {
                                         AND fa.estado=0
                                         AND fa.fecha BETWEEN ${fecha1} AND ${fecha2} 
                                         GROUP BY su.codigo, su.descripcion;`;
-		let productos = [];
-		for (let row of result.recordset) {
-			productos.push(row);
-		}
-		return productos;
-	} catch (err) {
-		console.log("ERROR!!! no se pudo realizar la consulta", err);
-		return [];
-	}
+    let productos = [];
+    for (let row of result.recordset) {
+      productos.push(row);
+    }
+    return productos;
+  } catch (err) {
+    console.log("ERROR!!! no se pudo realizar la consulta", err);
+    return [];
+  }
 };
 
 const calcularArqueoPorSucursal = async (
-	desde = new Date(),
-	hasta = new Date(),
-	codigoSucursal
+  desde = new Date(),
+  hasta = new Date(),
+  codigoSucursal
 ) => {
-	try {
-		const fecha1 = fechaQuery(desde);
-		const fecha2 = fechaQuery(hasta);
-		await sql.connect(sqlConfig);
-		const result =
-			await sql.query`SELECT su.codigo AS "codigosucursal", ltrim(rtrim(su.descripcion)) nombresucursal ,  SUM(a.cantidad) AS "cantidad", 
+  try {
+    const fecha1 = fechaQuery(desde);
+    const fecha2 = fechaQuery(hasta);
+    await sql.connect(sqlConfig);
+    const result =
+      await sql.query`SELECT su.codigo AS "codigosucursal", ltrim(rtrim(su.descripcion)) nombresucursal ,  SUM(a.cantidad) AS "cantidad", 
                                         SUM(pr.pcosto*a.cantidad) AS "totalCosto", 
                                         SUM((ROUND(a.precioneto,0))*a.cantidad) AS "totalVentas", 
                                         SUM(((ROUND(a.precioneto,0))*a.cantidad)-(pr.pcosto*a.cantidad)) AS "totalUtilidad"
@@ -196,28 +201,28 @@ const calcularArqueoPorSucursal = async (
                                         AND fa.fecha BETWEEN ${fecha1} AND ${fecha2} 
                                         AND su.codigo = ${codigoSucursal}
                                         GROUP BY su.codigo, su.descripcion;`;
-		let productos = [];
-		for (let row of result.recordset) {
-			productos.push(row);
-		}
-		return productos;
-	} catch (err) {
-		console.log("ERROR!!! no se pudo realizar la consulta", err);
-		return [];
-	}
+    let productos = [];
+    for (let row of result.recordset) {
+      productos.push(row);
+    }
+    return productos;
+  } catch (err) {
+    console.log("ERROR!!! no se pudo realizar la consulta", err);
+    return [];
+  }
 };
 
 const calcularVentasPorSucursal = async (
-	desde = new Date(),
-	hasta = new Date(),
-	codigoSucursal
+  desde = new Date(),
+  hasta = new Date(),
+  codigoSucursal
 ) => {
-	try {
-		const fecha1 = fechaQuery(desde);
-		const fecha2 = fechaQuery(hasta);
-		await sql.connect(sqlConfig);
-		const result =
-			await sql.query`SELECT su.codigo AS "codigosucursal", ltrim(rtrim(su.descripcion)) nombresucursal, SUM((ROUND(a.precioneto,0))*a.cantidad) AS "totalVentas"
+  try {
+    const fecha1 = fechaQuery(desde);
+    const fecha2 = fechaQuery(hasta);
+    await sql.connect(sqlConfig);
+    const result =
+      await sql.query`SELECT su.codigo AS "codigosucursal", ltrim(rtrim(su.descripcion)) nombresucursal, SUM((ROUND(a.precioneto,0))*a.cantidad) AS "totalVentas"
 FROM Facturadet a
 INNER JOIN Factura fa ON fa.id=a.idfactura
 LEFT OUTER JOIN STKTipoMov tm ON tm.id=fa.idstktipomov
@@ -230,23 +235,23 @@ AND fa.estado=0
 AND fa.fecha BETWEEN ${fecha1} AND ${fecha2} 
 AND su.codigo = ${codigoSucursal}
 GROUP BY su.codigo, su.descripcion;`;
-		let productos = [];
-		for (let row of result.recordset) {
-			productos.push(row);
-		}
-		return productos;
-	} catch (err) {
-		console.log("ERROR!!! no se pudo realizar la consulta", err);
-		return [];
-	}
+    let productos = [];
+    for (let row of result.recordset) {
+      productos.push(row);
+    }
+    return productos;
+  } catch (err) {
+    console.log("ERROR!!! no se pudo realizar la consulta", err);
+    return [];
+  }
 };
 
 module.exports = {
-	productosMasVendidosDelDia,
-	nuevosProductos,
-	buscarProductos,
-	buscarProductosCB,
-	calcularArqueo,
-	calcularArqueoPorSucursal,
-	calcularVentasPorSucursal,
+  productosMasVendidosDelDia,
+  nuevosProductos,
+  buscarProductos,
+  buscarProductosCB,
+  calcularArqueo,
+  calcularArqueoPorSucursal,
+  calcularVentasPorSucursal,
 };
